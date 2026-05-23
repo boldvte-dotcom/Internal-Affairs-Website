671a6877360329ed75c099775aa126f13cd0a815
boldvte-dotcom <rassimmehlil@gmail.com>
2026-05-22 22:14:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2372,4 +2372,7 @@ def permission_abuse_unsuspend(payload: dict[str, Any]):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
+
